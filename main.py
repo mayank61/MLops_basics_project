69edee1b5f35c48f902b5166a590b5bb358c9403
mayank61 <mayankteli6@gmail.com>
2025-07-31 23:20:59 +0530

    adding data transformation

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 STAGE_NAME="Data Ingestion stage"
 try:
-    logger.info(f">>>>> stage{STAGE_NAME} started>>>")
+    logger.info(f">>>>> stage{STAGE_NAME} started<<<<\n\n")
     obj=DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f">>>>> stage {STAGE_NAME} completed >>>")
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<< \n\n")
 except Exception as e:
     logger.exception(e)
 
 STAGE_NAME="Data Validation stage"
 try:
-    logger.info(f">>>>> stage {STAGE_NAME} started")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<\n\n")
     obj=DataValidationTrainingPipeline()
     obj.main()
-    logger.info(f">>>> {STAGE_NAME} completed ")
+    logger.info(f">>>>>>>>{STAGE_NAME} completed<<<<<\n\n ")
 except Exception as e:
     logger.exception(e)
 
-
+STAGE_NAME="Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<\n\n")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
