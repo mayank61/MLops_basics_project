943d583ad16827744a356642c1db726f9e2a07b4
mayank61 <mayankteli6@gmail.com>
2025-08-11 11:59:43 +0530

    adding model training pipeling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 STAGE_NAME="Data Ingestion stage"
 try:
     logger.info(f">>>>> stage{STAGE_NAME} started<<<<\n\n")
@@ -29,3 +30,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+STAGE_NAME="Model Trainig stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<\n\nx============x")
+    obj=ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> {STAGE_NAME} completed <<<< \n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
